a5908edb6953aaac44ead5f90db6d2dcee444da8
wdevin <75394687+wdevin710@users.noreply.github.com>
2022-06-09 09:10:05 -0400

    Delete CS 555 directory

--- CS 555/assignment 2/CS555_Assignment2.R
+++
@@ -1,9 +0,0 @@
-df1 <- read.csv("/Users/haowu/Desktop/Boston University Graduate Study/CS 555/assignment 2/Calorie.csv", header = FALSE)
-typeof(df1)
-names(df1) <- c("participants","non_participants")
-df2 <- df1[,c(1)]
-df3 <- df1[,c(2)]
-hist(df2, main="histogram of the participants", xlab = "Children's calories", ylab = "Frequency", border = "black", freq = TRUE)
-hist(df3, main="histogram of the non_participants", xlab = "Children's calories", ylab = "Frequency", border = "black", freq = TRUE)
-summary(df2)
-
