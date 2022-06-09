69180fbaa64bf1b9b68857c0f6eb5251a2ea4da6
wdevin <75394687+wdevin710@users.noreply.github.com>
2022-06-08 22:48:30 -0400

    Delete CS 524 directory

--- CS 524/Term Project/DefaultComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-//from textbook p364
-public class DefaultComparator<E> implements Comparator<E> {
-    public int compare(E a, E b) throws ClassCastException{
-        return ((Comparable<E>)a).compareTo(b);
-    }
-}
-    
-
